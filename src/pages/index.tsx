e42d8adda0f03951cd6fd18119a491052a6154a3
harmispatel <harmistest@gmail.com>
2022-11-15 19:25:11 +0530

    Create a new page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,19 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import * as React from 'react'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles: any = {
-  textAlign: "center",
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-
-const IndexPage: React.FC<PageProps> = () => {
+const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-    </main>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
+    </Layout>
   )
 }
 
-export default IndexPage
+export const Head = () => <title>Home Page</title>
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export default IndexPage
